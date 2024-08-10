8c0c3196dc88c1ec0bd59a68f0a101b32539f537
brent <brentrwestbrook@gmail.com>
2024-08-10 15:40:18 -0400

    impl text and xfont_free

--- src/drw.rs
+++ src/drw.rs
@@ -1,12 +1,14 @@
-use std::ffi::{c_char, c_int, c_uint, CStr};
-use std::ptr::null_mut;
+use std::ffi::{c_char, c_int, c_long, c_uint, CStr};
+use std::ptr::{null, null_mut};
 
-use crate::bindgen::Cur;
-use crate::bindgen::Drw;
-use crate::bindgen::Window;
+use fontconfig_sys::FcMatchPattern;
+
+use crate::bindgen::{self, Clr, Cur, Display, Drw, Fnt, Window};
+use crate::die;
 use crate::util::ecalloc;
-use crate::{bindgen, die};
-use bindgen::{Clr, Display, Fnt};
+
+// defined in drw.c
+const UTF_SIZ: usize = 4;
 
 pub(crate) fn create(
     dpy: *mut Display,
@@ -190,6 +192,19 @@ fn xfont_create(
     }
 }
 
+fn xfont_free(font: *mut Fnt) {
+    if font.is_null() {
+        return;
+    }
+    unsafe {
+        if !(*font).pattern.is_null() {
+            bindgen::FcPatternDestroy((*font).pattern);
+        }
+        bindgen::XftFontClose((*font).dpy, (*font).xfont);
+        libc::free(font.cast());
+    }
+}
+
 fn clr_create(drw: *mut Drw, dest: *mut Clr, clrname: *const c_char) {
     if drw.is_null() || dest.is_null() || clrname.is_null() {
         return;
@@ -242,18 +257,298 @@ pub(crate) fn fontset_getwidth(drw: *mut Drw, text: *const c_char) -> c_uint {
 }
 
 // DUMMY
-#[allow(clippy::too_many_arguments)]
+#[allow(clippy::too_many_arguments, unused)]
 pub(crate) fn text(
     drw: *mut Drw,
-    x: c_int,
+    mut x: c_int,
     y: c_int,
-    w: c_uint,
+    mut w: c_uint,
     h: c_uint,
     lpad: c_uint,
-    text: *const c_char,
+    mut text: *const c_char,
     invert: c_int,
 ) -> c_int {
-    unsafe { bindgen::drw_text(drw, x, y, w, h, lpad, text, invert) }
+    // this function is very confusing and likely can be dramatically simplified
+    // with Rust's native utf8 handling. For now, I'm declaring all of the
+    // variables at the top to match C as much as possible.
+    unsafe {
+        let i: c_int = 0;
+        let mut ty: c_int = 0;
+        let mut ellipsis_x: c_int = 0;
+
+        let mut tmpw: c_uint = 0;
+        let mut ew: c_uint = 0;
+        let mut ellipsis_w: c_uint = 0;
+        let mut ellipsis_len: c_uint = 0;
+
+        let mut d: *mut bindgen::XftDraw = null_mut();
+
+        let mut usedfont: *mut Fnt = null_mut();
+        let mut curfont: *mut Fnt = null_mut();
+        let mut nextfont: *mut Fnt = null_mut();
+
+        let mut utf8strlen: c_int = 0;
+        let mut utf8charlen: c_int = 0;
+        let render: c_int = (x != 0 || y != 0 || w != 0 || h != 0) as c_int;
+
+        let mut utf8codepoint: c_long = 0;
+
+        let mut utf8str: *const c_char = null();
+
+        let mut fccharset: *mut bindgen::FcCharSet = null_mut();
+        let mut fcpattern: *mut bindgen::FcPattern = null_mut();
+        let mut match_: *mut bindgen::FcPattern = null_mut();
+
+        let mut result: bindgen::XftResult = 0;
+
+        let mut charexists: c_int = 0;
+        let mut overflow: c_int = 0;
+
+        // keep track of a couple codepoints for which we have no match
+        const NOMATCHES_LEN: usize = 64;
+        struct NoMatches {
+            codepoint: [c_long; NOMATCHES_LEN],
+            idx: usize,
+        }
+        static mut NOMATCHES: NoMatches = NoMatches {
+            codepoint: [0; NOMATCHES_LEN],
+            idx: 0,
+        };
+        static mut ELLIPSIS_WIDTH: c_uint = 0;
+
+        if drw.is_null()
+            || (render != 0 && (*drw).scheme.is_null() || w == 0)
+            || text.is_null()
+            || (*drw).fonts.is_null()
+        {
+            return 0;
+        }
+
+        let drw = &mut *drw;
+        if render == 0 {
+            w = if invert != 0 { invert } else { !invert } as u32;
+        } else {
+            bindgen::XSetForeground(
+                drw.dpy,
+                drw.gc,
+                (*drw.scheme.add(if invert != 0 {
+                    bindgen::ColFg
+                } else {
+                    bindgen::ColBg
+                } as usize))
+                .pixel,
+            );
+            bindgen::XFillRectangle(drw.dpy, drw.drawable, drw.gc, x, y, w, h);
+            d = bindgen::XftDrawCreate(
+                drw.dpy,
+                drw.drawable,
+                bindgen::XDefaultVisual(drw.dpy, drw.screen),
+                bindgen::XDefaultColormap(drw.dpy, drw.screen),
+            );
+            x += lpad as i32;
+            w -= lpad;
+        }
+
+        usedfont = drw.fonts;
+        if ELLIPSIS_WIDTH == 0 && render != 0 {
+            ELLIPSIS_WIDTH = fontset_getwidth(drw, c"...".as_ptr());
+        }
+        'no_match: loop {
+            ew = 0;
+            ellipsis_len = 0;
+            utf8strlen = 0;
+            utf8str = text;
+            nextfont = null_mut();
+
+            // I believe this loop is just walking along the characters in text
+            // and computing their width. text += utf8charlen at the end just
+            // advances the pointer to the next codepoint, so in Rust I should
+            // be able to do something like text.chars() and replace almost all
+            // of this.
+            //
+            // NOTE this was almost an infinite loop because I translated
+            // `while(*text)` to `while !text.is_null()`, but we actually need
+            // to check if we're at the null byte at the end of the string, NOT
+            // if text is a null pointer
+            while *text != b'\0' as i8 {
+                utf8charlen =
+                    bindgen::utf8decode(text, &mut utf8codepoint, UTF_SIZ)
+                        as c_int;
+                curfont = drw.fonts;
+                while !curfont.is_null() {
+                    charexists = (charexists != 0
+                        || bindgen::XftCharExists(
+                            drw.dpy,
+                            (*curfont).xfont,
+                            utf8codepoint as u32,
+                        ) != 0) as c_int;
+                    if charexists != 0 {
+                        bindgen::drw_font_getexts(
+                            curfont,
+                            text,
+                            utf8charlen as u32,
+                            &mut tmpw,
+                            null_mut(),
+                        );
+                        if ew + ELLIPSIS_WIDTH <= w {
+                            // keep track where the ellipsis still fits
+                            ellipsis_x = x + ew as i32;
+                            ellipsis_w = w - ew;
+                            ellipsis_len = utf8strlen as c_uint;
+                        }
+
+                        if ew + tmpw > w {
+                            overflow = 1;
+                            // called from drw_fontset_getwidth_clamp():
+                            // it wants the width AFTER the overflow
+                            if render == 0 {
+                                x += tmpw as i32;
+                            } else {
+                                utf8strlen = ellipsis_len as c_int;
+                            }
+                        } else if curfont == usedfont {
+                            utf8strlen += utf8charlen;
+                            text = text.add(utf8charlen as usize);
+                            ew += tmpw;
+                        } else {
+                            nextfont = curfont;
+                        }
+                        break;
+                    }
+                    curfont = (*curfont).next;
+                }
+
+                if overflow != 0 || charexists == 0 || !nextfont.is_null() {
+                    break;
+                } else {
+                    charexists = 0;
+                }
+            } // end while(*text)
+
+            if utf8strlen != 0 {
+                if render != 0 {
+                    ty = y
+                        + (h - (*usedfont).h) as i32 / 2
+                        + (*(*usedfont).xfont).ascent;
+                    bindgen::XftDrawStringUtf8(
+                        d,
+                        drw.scheme.add(if invert != 0 {
+                            bindgen::ColBg
+                        } else {
+                            bindgen::ColFg
+                        } as usize),
+                        (*usedfont).xfont,
+                        x,
+                        ty,
+                        utf8str as *const bindgen::XftChar8,
+                        utf8strlen,
+                    );
+                }
+                x += ew as i32;
+                w -= ew;
+            }
+            if render != 0 && overflow != 0 {
+                self::text(
+                    drw,
+                    ellipsis_x,
+                    y,
+                    ellipsis_w,
+                    h,
+                    0,
+                    c"...".as_ptr(),
+                    invert,
+                );
+            }
+
+            if *text == b'\0' as i8 || overflow != 0 {
+                break;
+            } else if !nextfont.is_null() {
+                charexists = 0;
+                usedfont = nextfont;
+            } else {
+                // regardless of whether or not a fallback font is found, the
+                // character must be drawn
+                charexists = 1;
+
+                for i in 0..NOMATCHES_LEN {
+                    // avoid calling XftFontMatch if we know we won't find a
+                    // match
+                    if utf8codepoint == NOMATCHES.codepoint[i] {
+                        // goto no_match
+                        usedfont = drw.fonts;
+                        continue 'no_match;
+                    }
+                }
+
+                fccharset = bindgen::FcCharSetCreate();
+                bindgen::FcCharSetAddChar(fccharset, utf8codepoint as u32);
+
+                if (*drw.fonts).pattern.is_null() {
+                    // refer to the comment in xfont_create for more information
+                    die("the first font in the cache must be loaded from a font string");
+                }
+
+                fcpattern = bindgen::FcPatternDuplicate((*drw.fonts).pattern);
+                bindgen::FcPatternAddCharSet(
+                    fcpattern,
+                    // cast &[u8] to *u8 and then to *i8, hopefully okay
+                    bindgen::FC_CHARSET.as_ptr() as *const _,
+                    fccharset,
+                );
+                bindgen::FcPatternAddBool(
+                    fcpattern,
+                    // same as above: &[u8] -> *u8 -> *i8
+                    bindgen::FC_SCALABLE.as_ptr() as *const _,
+                    bindgen::FcTrue as i32,
+                );
+
+                bindgen::FcConfigSubstitute(
+                    null_mut(),
+                    fcpattern,
+                    FcMatchPattern,
+                );
+                bindgen::FcDefaultSubstitute(fcpattern);
+                match_ = bindgen::XftFontMatch(
+                    drw.dpy,
+                    drw.screen,
+                    fcpattern,
+                    &mut result,
+                );
+
+                bindgen::FcCharSetDestroy(fccharset);
+                bindgen::FcPatternDestroy(fcpattern);
+
+                if !match_.is_null() {
+                    usedfont = xfont_create(drw, null_mut(), match_);
+                    if !usedfont.is_null()
+                        && bindgen::XftCharExists(
+                            drw.dpy,
+                            (*usedfont).xfont,
+                            utf8codepoint as u32,
+                        ) != 0
+                    {
+                        curfont = drw.fonts;
+                        while !(*curfont).next.is_null() {
+                            curfont = (*curfont).next;
+                        }
+                        (*curfont).next = usedfont;
+                    } else {
+                        xfont_free(usedfont);
+                        NOMATCHES.idx += 1;
+                        NOMATCHES.codepoint[NOMATCHES.idx % NOMATCHES_LEN] =
+                            utf8codepoint;
+                        // no_match label
+                        usedfont = drw.fonts;
+                    }
+                }
+            }
+        }
+        if !d.is_null() {
+            bindgen::XftDrawDestroy(d);
+        }
+
+        x + if render != 0 { w } else { 0 } as i32
+    }
 }
 
 pub(crate) fn map(
