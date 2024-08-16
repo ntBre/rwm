801428452006bd9f4c1833ac9a227a1d6e7e4584
brent <brentrwestbrook@gmail.com>
2024-08-15 22:06:51 -0400

    try writing Button, but it's spiraling out of control
    some of the fields reference statics, so BUTTONS can't be const. but raw
    pointers aren't Sync so it can't be static either. I'm going to have to
    implement Args and Layout myself I think to get BUTTONS to work

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,13 @@ use std::{
     ptr::null,
 };
 
-use crate::bindgen::Rule;
+use x11::xlib::{Button1, Button2, Button3};
+
+use crate::bindgen;
+use crate::{
+    bindgen::{Arg, Button, Rule, MODKEY},
+    enums::Clk,
+};
 
 /// Border pixel of windows
 pub const BORDERPX: c_uint = 3;
@@ -44,4 +50,79 @@ impl Rule {
 pub const RULES: [Rule; 2] = [
     Rule::new(c"Gimp", null(), null(), 0, 1, -1),
     Rule::new(c"Firefox", null(), null(), 1 << 8, 0, -1),
+];
+
+impl Button {
+    const fn new(
+        click: Clk,
+        mask: c_uint,
+        button: c_uint,
+        func: unsafe extern "C" fn(*const Arg),
+        arg: Arg,
+    ) -> Self {
+        Self { click: click as c_uint, mask, button, func: Some(func), arg }
+    }
+}
+
+impl Arg {
+    const fn default() -> Self {
+        Self { i: 0 }
+    }
+}
+
+pub static BUTTONS: [Button; 11] = [
+    Button::new(
+        Clk::LtSymbol,
+        0,
+        Button1,
+        bindgen::setlayout,
+        Arg::default(),
+    ),
+    Button::new(
+        Clk::LtSymbol,
+        0,
+        Button3,
+        bindgen::setlayout,
+        // supposed to be a *const c_void, cast & to *const Layout then void
+        Arg { v: &bindgen::layouts[2] as *const _ as *const _ },
+    ),
+    Button::new(Clk::WinTitle, 0, Button2, bindgen::zoom, Arg::default()),
+    Button::new(
+        Clk::StatusText,
+        0,
+        Button2,
+        bindgen::spawn,
+        Arg { v: bindgen::termcmd.as_ptr().cast() },
+    ),
+    Button::new(
+        Clk::ClientWin,
+        MODKEY,
+        Button1,
+        bindgen::movemouse,
+        Arg::default(),
+    ),
+    Button::new(
+        Clk::ClientWin,
+        MODKEY,
+        Button2,
+        bindgen::togglefloating,
+        Arg::default(),
+    ),
+    Button::new(
+        Clk::ClientWin,
+        MODKEY,
+        Button3,
+        bindgen::resizemouse,
+        Arg::default(),
+    ),
+    Button::new(Clk::TagBar, 0, Button1, bindgen::view, Arg::default()),
+    Button::new(Clk::TagBar, 0, Button3, bindgen::toggleview, Arg::default()),
+    Button::new(Clk::TagBar, MODKEY, Button1, bindgen::tag, Arg::default()),
+    Button::new(
+        Clk::TagBar,
+        MODKEY,
+        Button3,
+        bindgen::toggletag,
+        Arg::default(),
+    ),
 ];
