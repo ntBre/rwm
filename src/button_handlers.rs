d8044e8d199dd965b11b8337eb1c9b956fd40d44
brent <brentrwestbrook@gmail.com>
2024-08-15 22:57:08 -0400

    impl view

--- src/button_handlers.rs
+++ src/button_handlers.rs
@@ -1,4 +1,10 @@
-use crate::Arg;
+#![allow(unused)]
+
+use std::ptr::null_mut;
+
+use crate::bindgen::selmon;
+
+use crate::{arrange, focus, Arg, TAGMASK};
 
 pub fn setlayout(arg: &Arg) {
     log::trace!("setlayout");
@@ -25,7 +31,19 @@ pub fn resizemouse(arg: &Arg) {
 }
 
 pub fn view(arg: &Arg) {
-    log::trace!("view");
+    log::trace!("view: {arg:?}");
+    let Arg::Uint(ui) = arg else { return };
+    unsafe {
+        if ui & TAGMASK == (*selmon).tagset[(*selmon).seltags as usize] {
+            return;
+        }
+        (*selmon).tagset[(*selmon).seltags as usize] ^= 1; // toggle sel tagset
+        if ui & TAGMASK != 0 {
+            (*selmon).tagset[(*selmon).seltags as usize] = ui & TAGMASK;
+        }
+        focus(null_mut());
+        arrange(selmon);
+    }
 }
 
 pub fn toggleview(arg: &Arg) {
