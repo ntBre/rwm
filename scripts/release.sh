da36fbbaa0f81e99d37c331fce84e1b6ab98b97e
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2026-01-16 22:11:33 -0500

    Set up to release with rooster and cargo-dist (#96)
    Looks like I can't trigger this yet, so I'll test a dry run after
    merging.

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Prepare for a release
+#
+# All additional options are passed to `rooster release`
+set -eu
+
+export UV_PREVIEW=1
+
+script_root="$(realpath "$(dirname "$0")")"
+project_root="$(dirname "$script_root")"
+
+echo "Updating metadata with rooster..."
+cd "$project_root"
+uvx --python 3.12 --isolated -- \
+    rooster@0.1.1 release "$@"
+
+echo "Updating lockfile..."
+cargo update -p rwm
